254f1d9df3658a769b54c16b6d30f827555cd7ec
chad fitzgerald <fitzchad@gmail.com>
2022-12-14 14:02:55 -0800

    Finish context test

--- practice-for-week-14-react-context-review-with-tests-long-practice-main/src/components/SetCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice-main/src/components/SetCoffeeBean.js
@@ -1,9 +1,19 @@
+import { CoffeeConsumer } from "../context/CoffeeContext";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const {coffeeBean, setCoffeeBeanId} = CoffeeConsumer();
+  const onSelect = (e) => {
+    console.log(e.target.value);
+    setCoffeeBeanId(e.target.value);
+  }
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
+        value={coffeeBean.id}
         name="coffee-bean"
+        onChange={onSelect}
       >
         {coffeeBeans.map(bean => (
           <option
