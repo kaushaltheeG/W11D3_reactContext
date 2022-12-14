276ef40e4c41d3b52d10ce1cd54f70b1a964e73f
chad fitzgerald <fitzchad@gmail.com>
2022-12-14 14:33:54 -0800

    Finish greenhouse phase 2

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/LightSwitch.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,20 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext';
 
 function LightSwitch() {
+  const { themeName, setThemeName } = useTheme();
+
+  const clickHandler = e => {
+    // console.log(e);
+    console.log(e.target.className);
+    if (e.target.className === "on") setThemeName('day');
+    else setThemeName('night');
+  }
+
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={clickHandler}>DAY</div>
+      <div className="off" onClick={clickHandler}>NIGHT</div>
     </div>
   );
 }
