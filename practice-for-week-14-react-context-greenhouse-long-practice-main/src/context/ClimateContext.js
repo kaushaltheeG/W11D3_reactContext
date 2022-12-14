e464dcb97719c7e6ec1b101ec01ef01751e14046
kaushaltheeG <kaushalsk09@gmail.com>
2022-12-14 14:46:53 -0800

    Finished greenhouse phase 3

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
@@ -1,2 +1,27 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { useState, useEffect, createContext, useContext } from "react";
+
+export const ClimateContext = createContext();
+
+export const ClimateProvider = (props) => {
+    const [temperature, setTemperature] = useState(50);
+
+    return (
+        <ClimateContext.Provider
+            value={{
+                temperature,
+                setTemperature
+            }}
+        >
+            {props.children}
+        </ClimateContext.Provider>
+    )
+}
+
+export const useTemp = () => useContext(ClimateContext);
+
+
+
+
